0820e4c7c9a46014c4e06cf7728e59f660f40e90
Duncan Robbins and patrick delaney <duncan@>
2020-01-30 15:51:47 -0800

    finalize indentation correction with semicolons

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,30 @@
 function dice() {
   var diceValue = Math.floor((Math.random() * 6) +1);
-  return diceValue
-};
+  return diceValue;
+}
+
 function ai() {
   for(i=0; i<2; ++i) {
-    var currentDie = dice()
+    var currentDie = dice();
     if(currentDie === 1) {
       $("#diceValue").text(currentDie);
-      turnBank = 0
+      turnBank = 0;
       $("#turnBank" + currentPlayer).text(turnBank);
       if(currentPlayer === 4) {
-      currentPlayer = 3
+      currentPlayer = 3;
       $("#roll-btn, #hold-btn").removeClass("playerTwo").addClass("playerOne");
-      break
+      break;
       }
     } else if(currentDie > 1) {
-      turnBank += currentDie
+      turnBank += currentDie;
       $("#diceValue").text(currentDie);
     } $("#turnBank2").text(turnBank);
   }
   if(currentPlayer === 4) {
-    totalBank1 += turnBank
-    turnBank = 0
+    totalBank1 += turnBank;
+    turnBank = 0;
     if(currentPlayer === 4) {
-      currentPlayer = 3
+      currentPlayer = 3;
       $("#roll-btn, #hold-btn").removeClass("playerOne").addClass("playerTwo");
     } 
     $("#turnBank2").text("0");
@@ -32,54 +33,56 @@ function ai() {
       alert("player 2 is the winner");
     }
   }
-};
-var totalBank = 0
-var totalBank1 = 0
-var totalBank2 = 0
-var turnBank = 0
-var currentPlayer = 1
-var currentTotalBank = totalBank
+}
+
+var totalBank = 0;
+var totalBank1 = 0;
+var totalBank2 = 0;
+var turnBank = 0;
+var currentPlayer = 1;
+var currentTotalBank = totalBank;
+
 $(document).ready(function(){
   $("#ai-btn").click(function(){
-    currentPlayer = 3
+    currentPlayer = 3;
   });
   $("#roll-btn").click(function(){
-    var currentDie = dice()
-    var currentPlayerTest = currentPlayer
+    var currentDie = dice();
+    var currentPlayerTest = currentPlayer;
     if(currentPlayer === 3) {
-      currentPlayerTest = 1
+      currentPlayerTest = 1;
     } 
     if(currentDie === 1) {
       $("#diceValue").text(currentDie);
-      turnBank = 0
+      turnBank = 0;
       $("#turnBank" + currentPlayerTest).text(turnBank);
       if(currentPlayer === 1) {
-        currentPlayer = 2
+        currentPlayer = 2;
         $("#roll-btn, #hold-btn").removeClass("playerOne").addClass("playerTwo");
       } else if(currentPlayer === 2) {
-        currentPlayer = 1
+        currentPlayer = 1;
         $("#roll-btn, #hold-btn").removeClass("playerTwo").addClass("playerOne");
       } else if(currentPlayer === 3) {
-        currentPlayer = 4
+        currentPlayer = 4;
         ai();
         $("#roll-btn, #hold-btn").removeClass("playerTwo").addClass("playerOne");
       }
     }
     else if(currentDie > 1) {
-      turnBank += currentDie
+      turnBank += currentDie;
       $("#diceValue").text(currentDie);
     }
     $("#turnBank" + currentPlayerTest).text(turnBank);
   });
   $("#hold-btn").click(function() {
     if(currentPlayer === 1) {
-     totalBank1 += turnBank
+     totalBank1 += turnBank;
     } else if(currentPlayer === 2) {
-      totalBank2 += turnBank
+      totalBank2 += turnBank;
     } else if(currentPlayer === 3) {
-      totalBank2 += turnBank
+      totalBank2 += turnBank;
     }
-    turnBank = 0
+    turnBank = 0;
     if(currentPlayer === 1) {
       $("#turnBank1").text("0");
       $("#totalBank1").text(totalBank1);
@@ -93,13 +96,13 @@ $(document).ready(function(){
     if (totalBank1 >= 100 || totalBank2 >= 100) {
       alert("player " + currentPlayer+ " is the winner");
     } if(currentPlayer === 1) {
-      currentPlayer = 2
+      currentPlayer = 2;
       $("#roll-btn, #hold-btn").removeClass("playerOne").addClass("playerTwo");
     } else if(currentPlayer === 2) {
-      currentPlayer = 1
+      currentPlayer = 1;
       $("#roll-btn, #hold-btn").removeClass("playerTwo").addClass("playerOne");
     } else if(currentPlayer === 3) {
-      currentPlayer = 4
+      currentPlayer = 4;
       ai();
       $("#roll-btn, #hold-btn").removeClass("playerTwo").addClass("playerOne");
     }
