4387ced18e313dbc881b15390f46807addf82e8c
Duncan Robbins and patrick delaney <patrick@>
2020-01-30 15:07:50 -0800

    add computer player functionality

--- js/scripts.js
+++ js/scripts.js
@@ -2,29 +2,85 @@ function dice() {
 var diceValue = Math.floor((Math.random() * 6) +1);
 return diceValue
 }
+function ai() {
+  console.log("ai is doing its turn");
+  
+  //roll twice then pass 
+  //roll twice
+  for(i=0; i<2; ++i) {
+    console.log("in the for lop");
+    
+    var currentDie = dice()
+    console.log(currentDie)
+    if (currentDie === 1) {
+      $("#diceValue").text(currentDie);
+      currentTurnBank = 0
+      $("#turnBank" + currentPlayer).text(currentTurnBank)
+  
+      if( currentPlayer === 4) {
+    currentPlayer = 3
+    $("#roll-btn, #hold-btn").removeClass("playerTwo").addClass("playerOne");
+    break
+  }
+    }else if (currentDie > 1) {
+    currentTurnBank += currentDie
+      $("#diceValue").text(currentDie);
+    }
+
+  $("#turnBank2").text(currentTurnBank)
+
+  }
+if(currentPlayer === 4){
+  totalBank1 += currentTurnBank
+  currentTurnBank = 0
+  if( currentPlayer === 4){
+    currentPlayer = 3
+    $("#roll-btn, #hold-btn").removeClass("playerOne").addClass("playerTwo");
+   }
+   $("#turnBank2").text("0")
+   $("#totalBank2").text(totalBank1);
+  if (totalBank1 >= 100 || totalBank2 >= 100){
+    alert("player" + currentPlayer + " is the winner")
+  }
+}
+
+}
 var totalBank = 0
 var totalBank1 = 0
 var totalBank2 = 0
 var turnBank = 0
 var currentPlayer = 1
-var currentTurnBank = turnBank + currentPlayer -1
-var currentTotalBank = totalBank + currentPlayer -1
+var currentTurnBank = turnBank
+var currentTotalBank = totalBank
 $(document).ready(function(){
+  $("#ai-btn").click(function(){
+    currentPlayer = 3
+  })
+
+
+
   $("#roll-btn").click(function(){
     var currentDie = dice()
+    var currentPlayerTest = currentPlayer
+    if(currentPlayer === 3 ){currentPlayerTest = 1}
     if (currentDie === 1) {
       $("#diceValue").text(currentDie);
       currentTurnBank = 0
-      $("#turnBank" + currentPlayer).text(currentTurnBank)
+      $("#turnBank" + currentPlayerTest).text(currentTurnBank)
     
     
     //end turn
-   if( currentPlayer === 1){
+   if( currentPlayer === 1) {
     currentPlayer = 2
     $("#roll-btn, #hold-btn").removeClass("playerOne").addClass("playerTwo");
    }
-   else if( currentPlayer === 2){
+   else if( currentPlayer === 2) {
     currentPlayer = 1
+    $("#roll-btn, #hold-btn").removeClass("playerTwo").addClass("playerOne");
+   }
+   else if( currentPlayer === 3) {
+    currentPlayer = 4
+    ai();
     $("#roll-btn, #hold-btn").removeClass("playerTwo").addClass("playerOne");
    }
     }
@@ -33,7 +89,7 @@ $(document).ready(function(){
       $("#diceValue").text(currentDie);
     }
  
-   $("#turnBank" + currentPlayer).text(currentTurnBank)
+   $("#turnBank" + currentPlayerTest).text(currentTurnBank)
     
     });
   $("#hold-btn").click(function(){
@@ -42,6 +98,9 @@ $(document).ready(function(){
      totalBank1 += currentTurnBank
      }
      else if( currentPlayer === 2){
+      totalBank2 += currentTurnBank
+     }
+     else if( currentPlayer === 3){
       totalBank2 += currentTurnBank
      }
     currentTurnBank = 0
@@ -54,7 +113,10 @@ $(document).ready(function(){
         $("#turnBank2").text("0")
         $("#totalBank2").text(totalBank2);
       }
-
+      else if( currentPlayer === 3){
+        $("#turnBank1").text("0")
+        $("#totalBank1").text(totalBank2);
+      }
     
     if (totalBank1 >= 100 || totalBank2 >= 100){
       alert("player" + currentPlayer + " is the winner")
@@ -67,6 +129,11 @@ $(document).ready(function(){
      }
      else if( currentPlayer === 2){
       currentPlayer = 1
+      $("#roll-btn, #hold-btn").removeClass("playerTwo").addClass("playerOne");
+     }
+     else if( currentPlayer === 3){
+      currentPlayer = 4
+      ai()
       $("#roll-btn, #hold-btn").removeClass("playerTwo").addClass("playerOne");
      }
   })
